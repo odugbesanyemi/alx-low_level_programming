1d1c2d1b9b45421026015d5f5d8a18850db8be09
odugbesanyemi <odugbesanyemi@gmail.com>
2022-10-26 00:20:52 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - uses printsf to output a code 
  *
- * Return  0 (success) 
+ * Return:  0 (success) 
  *
  */
 
 int main(void)
 {
-	printf("with proper grammar , but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }
