07dc896bd5447d64265a3872fd0c583cf209761d
odugbesanyemi <odugbesanyemi@gmail.com>
2022-10-26 00:14:57 -0700

    ,print f fumctioms

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - uses printsf to output a code 
+ *
+ * Return  0 (success) 
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar , but the outcome is a piece of art,");
+	return(0);
+}
