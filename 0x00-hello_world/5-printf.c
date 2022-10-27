3f2a09d3824996aee063fc6d232c4e7ddf87a9ae
odugbesanyemi <odugbesanyemi@gmail.com>
2022-10-27 13:19:24 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - uses printsf to output a code 
  *
  * Return:  0 (success) 
- *
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
